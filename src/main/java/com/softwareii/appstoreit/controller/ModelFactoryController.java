6170b755072f4f4c472954a91d00350cb61be5a6
Kevin Velasquez <Keviin.stiiven99@gmail.com>
2024-10-09 09:16:36 -0500

    Se agrega: -Archivo util para manejar persistencia -Clase model factory -Clase persistencia que ayudara a manejar la comunicacion entre los archivos y las clases

---
+++ src/main/java/com/softwareii/appstoreit/controller/ModelFactoryController.java
@@ -0,0 +1,15 @@
+package com.softwareii.appstoreit.controller;
+import com.softwareii.appstoreit.model.Bodega;
+
+public class ModelFactoryController {
+
+    Bodega bodega;
+
+    private static class SingletonHolder {
+        private final static ModelFactoryController eINSTANCE = new ModelFactoryController();
+    }
+    public static ModelFactoryController getInstance() {
+        return SingletonHolder.eINSTANCE;
+    }
+
+}
---
+++ src/main/java/com/softwareii/appstoreit/utils/Persistencia.java
@@ -0,0 +1,4 @@
+package com.softwareii.appstoreit.utils;
+
+public class Persistencia {
+}
